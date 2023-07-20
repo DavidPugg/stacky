877e917968d0661bdcc9681d0f46b38628907eef
DavidPugg <pusti.david@gmail.com>
2023-07-20 14:54:14 +0200

    add deleteTodo endpoint

--- data/todos.go
+++ data/todos.go
@@ -1,16 +1,29 @@
 package data
 
+import "fmt"
+
 type Todo struct {
 	ID   int
 	Text string
 }
 
+var todos = []Todo{
+	{ID: 1, Text: "Tidy up the house"},
+	{ID: 2, Text: "Buy groceries"},
+	{ID: 3, Text: "Walk the dog"},
+}
+
 func (d *Data) GetTodos() ([]Todo, error) {
-	todos := []Todo{
-		{ID: 1, Text: "Tidy up the house"},
-		{ID: 2, Text: "Buy groceries"},
-		{ID: 3, Text: "Walk the dog"},
+	return todos, nil
+}
+
+func (d *Data) DeleteTodo(ID int) error {
+	for i, todo := range todos {
+		if todo.ID == ID {
+			todos = append(todos[:i], todos[i+1:]...)
+			return nil
+		}
 	}
 
-	return todos, nil
+	return fmt.Errorf("Todo with ID %d not found", ID)
 }
--- handlers/todos.go
+++ handlers/todos.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"strconv"
+
 	"github.com/davidpugg/stacky/data"
 	"github.com/gofiber/fiber/v2"
 )
@@ -8,6 +10,7 @@ import (
 func (h *Handlers) registerTodoRoutes(c *fiber.App) {
 	c.Get("/", h.renderTodos)
 	c.Post("/addTodo", h.addTodo)
+	c.Delete("/deleteTodo/:id", h.deleteTodo)
 }
 
 func (h *Handlers) renderTodos(c *fiber.Ctx) error {
@@ -19,12 +22,26 @@ func (h *Handlers) renderTodos(c *fiber.Ctx) error {
 
 func (h *Handlers) addTodo(c *fiber.Ctx) error {
 	todos, _ := h.data.GetTodos()
-	ID := todos[len(todos)-1].ID + 1
+
+	var ID int
+	if len(todos) == 0 {
+		ID = 1
+	} else {
+		ID = todos[len(todos)-1].ID + 1
+	}
 
 	todo := data.Todo{
-		ID: ID,
+		ID:   ID,
 		Text: c.FormValue("todo"),
 	}
 
 	return renderPartial(c, "todo", todo)
+}
+
+func (h *Handlers) deleteTodo(c *fiber.Ctx) error {
+	ID, _ := strconv.Atoi(c.Params("id", "0"))
+
+	h.data.DeleteTodo(ID)
+
+	return c.Send(nil)
 }
