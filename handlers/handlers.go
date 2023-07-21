2d1bae84a289ee8996b4f068d41657207072cb9d
DavidPugg <pusti.david@gmail.com>
2023-07-21 10:57:09 +0200

    dynamic error pages

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -40,16 +40,36 @@ func renderPage(c *fiber.Ctx, view string, data interface{}, layout ...string) e
 	return c.Render(fmt.Sprintf("%s", view), data, l)
 }
 
-func sendTrigger(c *fiber.Ctx, status int, trigger string, message string) error {
-	alert, err := json.Marshal(fiber.Map{trigger: message})
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString("Something went wrong")
+func sendError(c *fiber.Ctx, status int, details string) error {
+	var message string
+	switch status {
+	case fiber.StatusInternalServerError:
+		message = "Internal server error"
+	case fiber.StatusNotFound:
+		message = "Page not found"
+	default:
+		message = "Something went wrong"
 	}
 
-	c.Status(status).Set("HX-Trigger", string(alert))
-	return c.SendString(message)
+	c.Set("HX-Redirect", fmt.Sprintf("/error?status=%d&message=%s&details=%s", status, message, details))
+	return c.SendStatus(status)
+}
+
+func sendTrigger(c *fiber.Ctx, status int, trigger string, message interface{}) error {
+	alert, err := json.Marshal(fiber.Map{trigger: message})
+	if err != nil {
+		return sendError(c, fiber.StatusInternalServerError, "Could not marshal alert")
+	}
+
+	c.Set("HX-Trigger", string(alert))
+	return c.SendStatus(status)
 }
 
 func showAlert(c *fiber.Ctx, status int, message string) error {
-	return sendTrigger(c, status, "showAlert", message)
+	value := fiber.Map{
+		"status":  status,
+		"message": message,
+	}
+
+	return sendTrigger(c, status, "showAlert", value)
 }
--- public/js/htmx/listeners.js
+++
@@ -1,9 +0,0 @@
-document.body.addEventListener('htmx:responseError', function (evt) {
-  if (evt.detail.xhr.status === 404) {
-    window.location.href = '/404';
-  }
-
-  if (evt.detail.xhr.status === 500) {
-    window.location.href = '/500';
-  }
-});
