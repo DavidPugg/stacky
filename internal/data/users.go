1884ea42ef1664e01314555bd9dba1dfed97f762
DavidPugg <pusti.david@gmail.com>
2023-08-10 19:14:35 +0200

    update user page styles

--- internal/data/users.go
+++ internal/data/users.go
@@ -6,14 +6,16 @@ import (
 )
 
 type User_DB struct {
-	ID        int    `json:"id" db:"id"`
-	Avatar    string `json:"avatar" db:"avatar"`
-	Username  string `json:"username" db:"username"`
-	Email     string `json:"email" db:"email"`
-	Password  string `json:"-" db:"password"`
-	CreatedAt string `json:"created_at" db:"created_at"`
-	UpdatedAt string `json:"-" db:"updated_at"`
-	Followed  bool   `json:"followed" db:"followed"`
+	ID             int    `json:"id" db:"id"`
+	Avatar         string `json:"avatar" db:"avatar"`
+	Username       string `json:"username" db:"username"`
+	Email          string `json:"email" db:"email"`
+	Password       string `json:"-" db:"password"`
+	CreatedAt      string `json:"created_at" db:"created_at"`
+	UpdatedAt      string `json:"-" db:"updated_at"`
+	Followed       bool   `json:"followed" db:"followed"`
+	FollowingCount int    `json:"following_count" db:"following_count"`
+	FollowersCount int    `json:"followers_count" db:"followers_count"`
 }
 
 type UserWithPosts struct {
@@ -22,15 +24,13 @@ type UserWithPosts struct {
 }
 
 const baseUsersQuery = `
-	SELECT *,
+	SELECT users.id, users.avatar, users.username, users.email, users.created_at, users.updated_at,
 	EXISTS (
 		SELECT 1
 		FROM follows AS f
 		WHERE f.followee_id = users.id
 		AND f.follower_id = ?
-	) AS followed
-	FROM users
-`
+	) AS followed `
 
 func (d *Data) CreateUser(avatar, username, email, password string) (int, error) {
 	result, err := d.DB.Exec("INSERT INTO users (avatar, username, email, password) VALUES (?, ?, ?, ?)", avatar, username, email, password)
@@ -55,7 +55,7 @@ func (d *Data) CreateUser(avatar, username, email, password string) (int, error)
 }
 
 func (d *Data) GetUserByID(authUserID, userID int) (*User_DB, error) {
-	query := baseUsersQuery + `WHERE id = ?`
+	query := baseUsersQuery + `FROM users WHERE id = ?`
 
 	user := &User_DB{}
 	err := d.DB.Get(user, query, authUserID, userID)
@@ -67,7 +67,7 @@ func (d *Data) GetUserByID(authUserID, userID int) (*User_DB, error) {
 }
 
 func (d *Data) GetUserByEmail(userID int, email string) (*User_DB, error) {
-	query := baseUsersQuery + `WHERE email = ?`
+	query := baseUsersQuery + `FROM users WHERE email = ?`
 
 	user := &User_DB{}
 	err := d.DB.Get(user, query, userID, email)
@@ -79,7 +79,7 @@ func (d *Data) GetUserByEmail(userID int, email string) (*User_DB, error) {
 }
 
 func (d *Data) GetUserByUsername(userID int, username string) (*User_DB, error) {
-	query := baseUsersQuery + `WHERE username = ?`
+	query := baseUsersQuery + `FROM users WHERE username = ?`
 
 	user := &User_DB{}
 	err := d.DB.Get(user, query, userID, username)
@@ -103,11 +103,20 @@ func (d *Data) GetUserWithPostsByUsername(userID int, username string) (*UserWit
 		postsChan <- posts
 	}()
 
-	query := baseUsersQuery + `WHERE username = ?`
+	query := baseUsersQuery + `,
+	COUNT(f.id) AS followers_count,
+	COUNT(f2.id) AS following_count
+	FROM users
+	LEFT JOIN follows AS f ON f.followee_id = users.id
+	LEFT JOIN follows AS f2 ON f2.follower_id = users.id
+	WHERE username = ?
+	GROUP BY users.id
+	`
 
 	user := &User_DB{}
 	err := d.DB.Get(user, query, userID, username)
 	if err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 
