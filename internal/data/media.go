9a175351408d1da6edf6935b1e87b75284a8b7f6
DavidPugg <pusti.david@gmail.com>
2023-08-18 09:45:45 +0200

    move create file logic to media data file

--- internal/data/media.go
+++ internal/data/media.go
@@ -4,27 +4,69 @@ import (
 	"fmt"
 	"image"
 	"image/jpeg"
+	"image/png"
 	"mime/multipart"
 	"os"
 	"path/filepath"
 
 	"github.com/google/uuid"
+	"github.com/oliamb/cutter"
 )
 
-func (d *Data) CreateMediaLocally(img image.Image, header *multipart.FileHeader) (string, error) {
-	randID := uuid.New().String()
-	ext := filepath.Ext(header.Filename)
+type CropData struct {
+	X      float32 `json:"x"`
+	Y      float32 `json:"y"`
+	Width  float32 `json:"width"`
+	Height float32 `json:"height"`
+}
 
-	imagePath := fmt.Sprintf("public/assets/images/%s%s", randID, ext)
+func (d *Data) CreateMediaLocally(img *multipart.FileHeader, cropData CropData) (string, error) {
+	var (
+		ext       = filepath.Ext(img.Filename)
+		randID    = uuid.New().String()
+		imagePath = fmt.Sprintf("public/assets/images/%s%s", randID, ext)
+		i         image.Image
+	)
 
-	file, err := os.Create(imagePath)
+	file, err := img.Open()
+	if err != nil {
+		fmt.Println(err)
+		return "", fmt.Errorf("Error saving image")
+	}
+
+	defer file.Close()
+
+	if ext == ".png" {
+		i, err = png.Decode(file)
+	} else {
+		i, _, err = image.Decode(file)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		return "", fmt.Errorf("Error saving image")
+	}
+
+	cimg, err := cutter.Crop(i, cutter.Config{
+		Width:  int(cropData.Width),
+		Height: int(cropData.Height),
+		Anchor: image.Point{int(cropData.X), int(cropData.Y)},
+		Mode:   cutter.TopLeft,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return "", fmt.Errorf("Error sa ving image")
+	}
+
+	newFile, err := os.Create(imagePath)
 	if err != nil {
 		return "", err
 	}
 
 	defer file.Close()
 
-	err = jpeg.Encode(file, img, nil)
+	err = jpeg.Encode(newFile, cimg, nil)
 	if err != nil {
 		return "", err
 	}
